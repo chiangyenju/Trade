8a427a83e98dc1f63ca11b473414a421d06b084c
Yen Ju Andrew Chiang <37291863+chiangyenju@users.noreply.github.com>
2023-02-09 19:48:39 +0800

    bb

--- vsc.py
+++ vsc.py
@@ -1,35 +1,83 @@
+
 import websocket
 import json
 import pandas as pd
-
+import ta
+import matplotlib.pyplot as plt
+import math
 
 endpoint = 'wss://stream.binance.com:9443/ws'
 
-data = json.dumps({'method':'SUBSCRIBE','params':['ethusdt@kline_1m'],'id':1})
+symbol = 'ethusdt'
+rate = 5
+
+data = json.dumps({'method':'SUBSCRIBE','params':[symbol + '@kline_1m'],'id':1})
 
 df = pd.DataFrame()
 
+#indicators
+def get_sma(close_p, rate = 30):
+    return close_p.rolling(rate).mean()
+
+def get_bollinger_bands(close_p, sma, rate = 30):
+    std = close_p.rolling(rate).std()
+    bband_up = sma + 2 * std
+    bband_down = sma - 2 * std
+    return bband_up, bband_down
 
 
 def on_open(ws):
     ws.send(data)
 
 def on_message(ws, message):
-    global df, in_position
+    global df, rate
+    
+    #get raw data
     out = json.loads(message)
     out = pd.DataFrame({'open':float(out['k']['o']),
                         'close':float(out['k']['c']),
                         'high':float(out['k']['h']),
                         'low':float(out['k']['l']),
                         'b_vol':float(out['k']['v']),
-                        'q_vol':float(out['k']['q']),
-                        'closed':bool(out['k']['x'])
+                        'closed':bool(out['k']['x']),
                         },
                         index=[pd.to_datetime(out['E'], unit = 'ms')])
-    df = pd.concat([df,out], axis = 0)
-    df = df.tail(5)
-    print(df)
+
+
     
+    df = pd.concat([df,out], axis = 0)
+    
+    if df.shape[0] < rate:
+        sma = df['close']
+        bband_up, bband_down = df['close']
+        df['sma'] = sma
+        df['bb_u'] = bband_up
+        df['bb_d'] = bband_down
+    else:
+        sma = get_sma(df['close'], rate)
+        bband_up, bband_down = get_bollinger_bands(df['close'], sma, rate)
+        df['sma'] = sma
+        df['bb_u'] = bband_up
+        df['bb_d'] = bband_down
+    
+    
+    print(df)
+    # df = df.tail(5)
+    
+    # df.drop(df[df['closed'] == False].index, axis=0, inplace=True)
+    
+    
+    # plt.title(symbol + ' SMA_' + rate)
+    # plt.xlabel('time')
+    # plt.ylabel('Closing Prices')
+    # plt.plot(df.close, label='Closing Prices')
+    # plt.plot(sma, label= rate + ' SMA')
+    # plt.plot(bband_up, label='Bollinger Up', c='g')
+    # plt.plot(bband_down, label='Bollinger Down', c='r')
+    # plt.legend()
+    # plt.show()
+    
+
 
 
 ws = websocket.WebSocketApp(endpoint, on_message = on_message, on_open = on_open)
